5a01001deece709b2693892f72f235bc631dff59
radioactive1014 <email_id>
2017-07-13 08:50:19 -0400

    check

---
+++ CMakeFiles/object_click.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/object_click.dir/object_detection_click.cpp.o"
+  "object_click.pdb"
+  "object_click"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/object_click.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ readme.txt~
@@ -0,0 +1,4 @@
+red object detection code 
+
+object: Object detection. Gives the pixel position. 
+objec_click:  object detection + get pixel position of any point with the mouse click
